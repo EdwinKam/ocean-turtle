9c697d10055e3c8b0f2d241794f1f60fab63ca7e
ekam <ekam@netflix.com>
2024-12-12 00:15:19 -0800

    a loading screen

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -4,6 +4,7 @@ import { theme } from "@/constants/theme";
 import { hp, wp } from "@/lib/common";
 import PostCard from "@/components/PostCard";
 import { Post } from "@/model/post";
+import LoadingPostCard from "./LoadingPostCard";
 
 type PostListProps = {
   posts: Post[];
@@ -11,21 +12,32 @@ type PostListProps = {
 };
 
 const PostList = ({ posts, showCreationDate }: PostListProps) => {
-  // Sort posts if sortByCreationTs is true
-  if (showCreationDate) {
-    posts = [...posts].sort((a, b) => b.creationTs - a.creationTs);
-  }
+  // Sort posts if showCreationDate is true
+  const sortedPosts = showCreationDate
+    ? [...posts].sort((a, b) => b.creationTs - a.creationTs)
+    : posts;
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item.id} // Use a unique identifier if available
-        numColumns={2}
-        renderItem={({ item }) => (
-          <PostCard post={item} showCreationDate={showCreationDate} />
-        )}
-      />
+      {sortedPosts.length === 0 ? (
+        // Show loading cards when there are no posts
+        <FlatList
+          data={[...Array(20).keys()]} // Create an array to render multiple loading cards
+          keyExtractor={(item) => item.toString()}
+          numColumns={2}
+          renderItem={() => <LoadingPostCard />}
+        />
+      ) : (
+        // Show real posts when available
+        <FlatList
+          data={sortedPosts}
+          keyExtractor={(item) => item.id} // Use a unique identifier if available
+          numColumns={2}
+          renderItem={({ item }) => (
+            <PostCard post={item} showCreationDate={showCreationDate} />
+          )}
+        />
+      )}
     </View>
   );
 };
